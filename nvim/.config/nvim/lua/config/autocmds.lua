8f1718168d09d24151bd0b713de1de88ba87a568
amoryn <hello.amoryn@gmail.com>
2026-06-17 15:35:55 +0200

    feat(nvim): enable auto save

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,1 +1,18 @@
--- Add local autocmds here.
+local autosave = vim.api.nvim_create_augroup("amoryn_autosave", { clear = true })
+
+local function save_buffer()
+  if vim.bo.buftype ~= "" or not vim.bo.modifiable or vim.bo.readonly then
+    return
+  end
+
+  if vim.api.nvim_buf_get_name(0) == "" or not vim.bo.modified then
+    return
+  end
+
+  vim.cmd("silent! noautocmd write")
+end
+
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertLeave", "CursorHold" }, {
+  group = autosave,
+  callback = save_buffer,
+})
